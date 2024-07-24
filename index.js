d3ad698e051ce70b81aca0b00ecfaa816b95cfa0
SgTvmp <sg.tvmpzone@gmail.com>
2024-07-24 21:23:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("header").innerHTML = "hello Abi";
+
+var x = 1;
+var y = 2;
+document.getElementById("demo").innerHTML = x + y;
+document.getElementsByTagName("p")[0].innerHTML = "Milk";
